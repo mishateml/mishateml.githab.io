4d6a66ff4c21068e964bdbd572332d15749b67cd
mishateml <misha.teml@gmail.com>
2017-05-13 00:17:55 +0300

    start modul

---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="he">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <title>SapirFish</title>
+</head>
+<body>
+    <nav class=" navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <a href="index.php" class="navbar-brand"> <span class="glyphicon glyphicon-cutlery"></span> Sapir Fish Store</a>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">freshFish<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role = "menu">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                    </ul>
+                </li> <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">freshFish<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role = "menu">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+<!--header logo-->
+<div id="headerWrapper">
+    <div id="backPic"></div>
+    <div id="logoText"></div>
+    <div id="forFlower"></div>
+</div>
+
+    <div class="container-fluid">
+        <!--LeftBar-->
+        <div class="col-md-2">
+            left bar
+        </div>
+        <!--MainCont-->
+        <div class="col-md-8">
+
+                <div class="row">
+                    <h2 class="text-center"> fresh fish  </h2>
+                    <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish1.png" alt="Livase" class="productImg" />
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish2.png" alt="Livase" class="productImg"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish3.png" alt="Livase" class="productImg"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish5.png" alt="Livase" class="productImg"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish6.png" alt="Livase" class="productImg"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish7.png" alt="Livase" class="productImg"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish8.png" alt="Livase" class="productImg
+"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish9.png" alt="Livase" class="productImg
+"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish1.png" alt="Livase" class="productImg
+"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish2.png" alt="Livase" class="productImg
+"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish3.png" alt="Livase" class="productImg
+"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+ <div class="col-md-3">
+                        <h4>sea fish's </h4>
+                        <img src="images/products/fish5.png" alt="Livase" class="productImg
+"/>
+                        <p class="list-price text-danger"> <s> 99.99 </s>מחיר רגיל: </p>
+                        <P class="price">מחיר שלנו: 59.99</P>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-trget=" #detalls-1"> פרטים</button>
+                    </div>
+
+
+
+
+                </div>
+        </div>
+        <!--rigth side-->
+        <div class="col-md-2">
+            right
+        </div>
+    </div>
+
+    <footer class="text-center"  id="footer">&copy; זכויות שמורות <?php echo date("Y");?> מיכאל ויינשטיין </footer>
+
+<script>
+        jQuery(window).scroll(function () {
+        var vscroll = jQuery(this).scrollTop();
+        jQuery('#logoText').css({
+        "transform": "translate(0px, "+vscroll/2+"px)"
+    });
+    });
+        jQuery(window).scroll(function () {
+            var vscroll = jQuery(this).scrollTop();
+            jQuery('#backPic').css({
+                "transform": "translate("+vscroll/8+"px, "+vscroll/10+"px)"
+            });
+        });
+        jQuery(window).scroll(function () {
+            var vscroll = jQuery(this).scrollTop();
+            jQuery('#forFlower').css({
+                "transform": "translate(0px, -"+vscroll/2+"px)"
+            });
+        });
+
+
+</script>
+</body>
+</html>
